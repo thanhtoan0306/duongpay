234a47bb205dec2b9ce2394866301a248fa4a2b3
Toan Duong <toandt@nexlesoft.com>
2023-09-03 12:38:28 +0700

    adjust style

--- src/container/HomePage/index.js
+++ src/container/HomePage/index.js
@@ -4,7 +4,7 @@ import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>
+    <div style={{marginTop: 20}}>
       <div className="content-container">
         <div className="content">
           <div className="myname">HI, I'M A WEB DEVELOPER</div>
@@ -12,20 +12,18 @@ const HomePage = () => {
           <div className="major">"I have 3 years of experience in coding, with a focus on frontend web development as my major."</div>
           <div className="cv-button">
             <div>
-              <span className="prefix">
-                {/* <DownloadIcon /> */}
-              </span>
+              <span className="prefix">{/* <DownloadIcon /> */}</span>
               <span className="button-label"> MY CV</span>
             </div>
           </div>
         </div>
-      </div>
-      <div className="image-box">
-        <div className="image-container">
-          {/* <img src="/images/duongpay-black.jpg" /> */}
+        <div className="image-box">
+          <div className="image-container">
+            <img alt="" src="/images/duongpay.jpg" />
+          </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
