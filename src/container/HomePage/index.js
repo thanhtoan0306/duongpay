62d2e42b67f17992103f0c05185b2ad0d114b07c
toan <thanhtoan0306@gmail.com>
2023-09-23 10:16:11 +0700

    clean homepage

--- src/container/HomePage/index.js
+++ src/container/HomePage/index.js
@@ -1,31 +1,9 @@
 import React from "react";
-// import { DownloadIcon } from "@/public/custom-icons/custom-icons";
-import "./HomePage.css"
 
 const HomePage = () => {
   return (
-    <div style={{marginTop: 20}}>
-      <div className="content-container">
-        <div className="content">
-          <div className="myname">HI, I'M A WEB DEVELOPER</div>
-          <div className="job">My name is Duong Thanh Toan</div>
-          <div className="major">"I have 3 years of experience in coding, with a focus on frontend web development as my major."</div>
-          <div className="cv-button">
-            <div>
-              <span className="prefix">{/* <DownloadIcon /> */}</span>
-              <span className="button-label"> MY CV</span>
-            </div>
-          </div>
-        </div>
-        <div className="image-box">
-          <div className="image-container">
-            <img alt="" src="/images/duongpay.jpg" />
-          </div>
-        </div>
-      </div>
-      <div style={{height: 200, width: '100%', background: 'gray', marginTop: 20}}>
-        my skill
-      </div>
+    <div>
+
     </div>
   );
 };
